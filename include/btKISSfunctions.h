9d64546daf986b4abd651f2d2f0a64647204af38
Karl Berger <W4KRL@arrl.net>
2025-07-01 18:00:12 -0400

    Fix header guard directive in btKISSfunctions.h

--- include/btKISSfunctions.h
+++ include/btKISSfunctions.h
@@ -1,4 +1,4 @@
-#ifndef BTKISSFUNCTIONS_H  // MUST be #ifndef (not #ifdef)
+#ifndef BTKISSFUNCTIONS_H
 #define BTKISSFUNCTIONS_H
 
 #include <Arduino.h>
@@ -6,9 +6,9 @@
 
 extern BluetoothSerial BTSerial; // Bluetooth KISS Interface
 
-void setupBluetooth(); // Function to initialize Bluetooth Serial communication
-void sendKISSpacket(uint8_t *data, size_t len);	
-void transmitAFSK(uint8_t *packet, size_t len);
-void receiveAFSK();	
+void setupBluetooth();							// Function to initialize Bluetooth Serial communication
+void sendKISSpacket(uint8_t *data, size_t len); // Function to send KISS packet over Bluetooth Serial
+void transmitAFSK(uint8_t *packet, size_t len); // Function to transmit AFSK modulated data
+void receiveAFSK();								// Function to receive AFSK modulated data
 
 #endif // BTKISSFUNCTIONS_H
