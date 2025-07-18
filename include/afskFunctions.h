18e11c6ab41bd21c6b4d3e283841ce42daecd87b
Karl Berger <W4KRL@arrl.net>
2025-07-17 20:22:03 -0400

    Improve comments

--- include/afskFunctions.h
+++ include/afskFunctions.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-void setupAFSK();								   // Function to set up AFSK modulation settings
-void transmitAX25(uint8_t *kissFrame, size_t len); // Function to transmit AX.25 frames using AFSK modulation
+void setupAFSK();								                   // Call in setup() to configure AFSK modulation settings
+void transmitAX25(uint8_t *kissFrame, size_t len); // Call in loop() to transmit AX.25 frames using AFSK modulation
 
 #endif // AFSKFUNCTIONS_H
